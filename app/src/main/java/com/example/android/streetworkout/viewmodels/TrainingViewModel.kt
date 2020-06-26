25036a56e35302ff2f9385040a8690f9b69be809
steeshock <steeshock@gmail.com>
2020-06-26 22:06:51 +0300

    minor UI update for main views

--- app/src/main/java/com/example/android/streetworkout/viewmodels/TrainingViewModel.kt
+++ app/src/main/java/com/example/android/streetworkout/viewmodels/TrainingViewModel.kt
@@ -1,13 +1,6 @@
 package com.example.android.streetworkout.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TrainingViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "Тренировки"
-    }
-    val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/android/streetworkout/views/FavoritesFragment.kt
+++ app/src/main/java/com/example/android/streetworkout/views/FavoritesFragment.kt
@@ -1,11 +1,11 @@
 package com.example.android.streetworkout.views
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.example.android.streetworkout.R
 import com.example.android.streetworkout.databinding.FragmentFavoritesBinding
 import com.example.android.streetworkout.viewmodels.FavoritesViewModel
 
@@ -28,4 +28,40 @@ class FavoritesFragment : Fragment() {
 
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.activity_menu, menu)
+
+        val myActionMenuItem = menu.findItem(R.id.action_search)
+
+        var searchView = myActionMenuItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (!searchView.isIconified) {
+                    searchView.isIconified = true
+                }
+                myActionMenuItem.collapseActionView()
+                return false
+            }
+
+            override fun onQueryTextChange(s: String?): Boolean {
+                return false
+            }
+        })
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_search -> {
+                true
+            }
+            R.id.action_map -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
