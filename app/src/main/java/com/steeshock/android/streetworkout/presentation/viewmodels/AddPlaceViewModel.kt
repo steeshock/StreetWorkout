68b4b7a6caa249d11cf02da25b9bef7996e0ccf1
Eugeny Nikolaev <steeshock@gmail.com>
2022-02-05 00:38:29 +0300

    migrate binding logic to View+VM for add place

--- app/src/main/java/com/steeshock/android/streetworkout/presentation/viewmodels/AddPlaceViewModel.kt
+++ app/src/main/java/com/steeshock/android/streetworkout/presentation/viewmodels/AddPlaceViewModel.kt
@@ -1,7 +1,7 @@
 package com.steeshock.android.streetworkout.presentation.viewmodels
 
+import android.content.Intent
 import android.net.Uri
-import androidx.databinding.ObservableBoolean
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,6 +10,7 @@ import com.steeshock.android.streetworkout.data.model.Category
 import com.steeshock.android.streetworkout.data.model.Place
 import com.steeshock.android.streetworkout.data.repository.interfaces.ICategoriesRepository
 import com.steeshock.android.streetworkout.data.repository.interfaces.IPlacesRepository
+import com.steeshock.android.streetworkout.presentation.viewStates.AddPlaceViewState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -20,29 +21,48 @@ class AddPlaceViewModel @Inject constructor(
     private val categoriesRepository: ICategoriesRepository,
 ) : ViewModel() {
 
-    val allCategoriesLive: LiveData<List<Category>> = categoriesRepository.allCategories
-    val loadCompleted: MutableLiveData<Boolean> = MutableLiveData(false)
-    var sendingProgress: MutableLiveData<Int> = MutableLiveData(0)
+    private val mutableViewState: MutableLiveData<AddPlaceViewState> = MutableLiveData()
+    val viewState: LiveData<AddPlaceViewState>
+        get() = mutableViewState
+
+    val allCategories: LiveData<List<Category>> = categoriesRepository.allCategories
 
     var checkedCategoriesArray: BooleanArray? = null
     var selectedCategories: ArrayList<Int> = arrayListOf()
 
-    var selectedImages: MutableList<Uri> = mutableListOf()
-    var downloadedImagesLinks: ArrayList<String> = arrayListOf()
-    var selectedImagesMessage = ""
-
-    var isImagePickingInProgress: ObservableBoolean = ObservableBoolean(false)
-    var isLocationInProgress: ObservableBoolean = ObservableBoolean(false)
-    var isSendingInProgress: ObservableBoolean = ObservableBoolean(false)
+    private var selectedImages: MutableList<Uri> = mutableListOf()
+    private var downloadedImagesLinks: ArrayList<String> = arrayListOf()
 
     var sendingPlace: Place? = null
 
-    fun uploadDataToFirebase(uri: Uri, placeUUID: String) = viewModelScope.launch() {
+    fun onAddNewPlace(
+        place: Place,
+        onSuccessValidation: () -> Unit = {},
+    ) {
+        sendingPlace = place
+        mutableViewState.setNewState {
+            copy(
+                isSendingInProgress = true,
+                sendingProgress = 0,
+                maxProgressValue = selectedImages.size + 1
+            )
+        }
+
+        if (selectedImages.size > 0) {
+            selectedImages.forEach { uri ->
+                uploadDataToFirebase(uri, place.place_uuid)
+            }
+        } else {
+            createAndPublishNewPlace()
+        }
+    }
+
+    private fun uploadDataToFirebase(uri: Uri, placeUUID: String) = viewModelScope.launch() {
 
         val result = placesRepository.uploadImage(uri, placeUUID)
         downloadedImagesLinks.add(result.toString())
 
-        sendingProgress.postValue(sendingProgress.value?.plus(1))
+        mutableViewState.setNewState(postValue = true) { copy(sendingProgress = ++sendingProgress) }
         delay(500)
 
         if (downloadedImagesLinks.size == selectedImages.size) {
@@ -50,7 +70,7 @@ class AddPlaceViewModel @Inject constructor(
         }
     }
 
-    fun createAndPublishNewPlace() = viewModelScope.launch(Dispatchers.IO) {
+    private fun createAndPublishNewPlace() = viewModelScope.launch(Dispatchers.IO) {
 
         sendingPlace?.images = downloadedImagesLinks
 
@@ -59,10 +79,73 @@ class AddPlaceViewModel @Inject constructor(
             placesRepository.insertPlaceRemote(it)
         }
 
-        sendingProgress.postValue(sendingProgress.value?.plus(1))
+        mutableViewState.setNewState(postValue = true) {
+            copy(
+                sendingProgress = ++sendingProgress
+            )
+        }
+
         delay(300)
 
-        loadCompleted.postValue(true)
-        isSendingInProgress.set(false)
+        mutableViewState.setNewState(postValue = true) {
+            copy(
+                loadCompleted = true,
+                isSendingInProgress = false,
+                selectedImagesMessage ="",
+            )
+        }
+    }
+
+    fun onResetFields() {
+        selectedImages.clear()
+        selectedCategories.clear()
+        downloadedImagesLinks.clear()
+        checkedCategoriesArray = BooleanArray(allCategories.value?.size!!)
+        mutableViewState.setNewState(postValue = true) {
+            copy(
+                loadCompleted = false,
+                selectedImagesMessage = "",
+            )
+        }
+    }
+
+    fun onImagePicked(data: Intent?) {
+        selectedImages.add(data?.data!!)
+        mutableViewState.setNewState {
+            copy(
+                isImagePickingInProgress = false,
+                selectedImagesMessage = "Прикреплено фотографий: ${selectedImages.size}",
+            )
+        }
+    }
+
+    fun onOpenedImagePicker(isPickerVisible: Boolean) {
+        mutableViewState.setNewState {
+            copy(
+                isImagePickingInProgress = isPickerVisible,
+            )
+        }
+    }
+
+    fun onLocationProgressChanged(isLocationInProgress: Boolean) {
+        mutableViewState.setNewState {
+            copy(
+                isLocationInProgress = isLocationInProgress,
+            )
+        }
+    }
+
+    private fun MutableLiveData<AddPlaceViewState>.setNewState(
+        postValue: Boolean = false,
+        block: AddPlaceViewState.() -> AddPlaceViewState,
+    ) {
+        val currentState = value ?: AddPlaceViewState()
+        val newState = currentState.run { block() }
+
+        if (postValue) {
+            postValue(newState)
+        } else {
+            value = newState
+        }
     }
 }
