7e3d858b33078665dbc68b1842d140fe85c6de78
Eugeny Nikolaev <steeshock@gmail.com>
2022-08-14 10:42:12 +0300

    Add Lifecycle, Maps , Coroutines, Tests libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,15 +42,20 @@ dependencies {
 
     implementation(projects.design)
 
-    // Retrofit
     implementation(libs.bundles.retrofit)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.bundles.kotlinx.coroutines)
+    testImplementation(testLibs.bundles.unitTests)
+    testImplementation(testLibs.bundles.instrumentationTests)
 
     // Room
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
     kapt(libs.room.compiler)
 
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    // Maps and location
+    implementation(libs.playServices.maps)
+    implementation(libs.playServices.location)
 
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:28.2.0"))
@@ -67,43 +72,16 @@ dependencies {
     // Support Libraries
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.6.0")
-    implementation ("androidx.core:core-ktx:1.7.0")
     implementation ("androidx.legacy:legacy-support-v4:1.0.0")
-
-    // ConstraintLayout
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-
-    // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
 
     // Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
 
-    // RecyclerView
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
-
-    // Maps and location
-    implementation("com.google.android.gms:play-services-maps:18.0.2")
-    implementation("com.google.android.gms:play-services-location:19.0.1")
-
-    // Testing
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("org.mockito:mockito-android:3.12.4")
-    testImplementation("org.mockito:mockito-core:3.12.4")
-
     //For runBlockingTest, CoroutineDispatcher etc.
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.2")
     //For InstantTaskExecutorRule
     testImplementation("androidx.arch.core:core-testing:2.1.0")
-
-    //Kotlin Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.4.3")
 
     // Dagger
     implementation("com.google.dagger:dagger:2.40.5")
@@ -122,4 +100,6 @@ dependencies {
 
     implementation("com.github.denzcoskun:ImageSlideshow:0.1.0")
     implementation("com.github.dhaval2404:imagepicker:2.1")
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,10 +8,10 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-
             // region Versions
             version("room", "2.4.2")
             version("retrofit", "2.9.0")
+            version("coroutines", "1.6.1")
             // endregion
 
             // region Room
@@ -26,6 +26,33 @@ dependencyResolutionManagement {
             library("adapter-rxjava2", "com.squareup.retrofit2", "adapter-rxjava2").versionRef("retrofit")
             bundle("retrofit", listOf("retrofit", "converter-gson", "adapter-rxjava2"))
             // endregion
+
+            // region Lifecycle
+            library("lifecycle-extensions", "androidx.lifecycle:lifecycle-extensions:2.2.0")
+            library("lifecycle-viewmodel", "androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+            bundle("lifecycle", listOf("lifecycle-extensions", "lifecycle-viewmodel"))
+            // endregion
+
+            // region Maps and location
+            library("playServices-maps", "com.google.android.gms:play-services-maps:18.1.0")
+            library("playServices-location", "com.google.android.gms:play-services-location:20.0.0")
+            // endregion
+
+            // Kotlin Coroutines
+            library("kotlinx-coroutines-core", "org.jetbrains.kotlinx", "kotlinx-coroutines-core").versionRef("coroutines")
+            library("kotlinx-coroutines-android", "org.jetbrains.kotlinx", "kotlinx-coroutines-android").versionRef("coroutines")
+            library("kotlinx-coroutines-playServices", "org.jetbrains.kotlinx", "kotlinx-coroutines-play-services").versionRef("coroutines")
+            bundle("kotlinx-coroutines", listOf("kotlinx-coroutines-core", "kotlinx-coroutines-android", "kotlinx-coroutines-playServices"))
+            // endregion
+        }
+        create("testLibs") {
+            library("junit4", "junit:junit:4.13.2")
+            library("mockito-core", "org.mockito:mockito-core:3.12.4")
+            library("junit-ext", "androidx.test.ext:junit:1.1.3")
+            library("espresso-core", "androidx.test.espresso:espresso-core:3.4.0")
+            library("mockito-android", "org.mockito:mockito-android:3.12.4")
+            bundle("unitTests", listOf("junit4", "mockito-core"))
+            bundle("instrumentationTests", listOf("junit-ext", "espresso-core", "mockito-android"))
         }
     }
 }
